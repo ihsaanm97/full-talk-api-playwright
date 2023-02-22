bab76b6b1cb05360d9301323aed9508794a6f6e8
Ihsaan Muhiyadheen <ihsaan.seyed@anywhere.co>
2023-02-22 11:56:29 +0530

    uncommented a line in wdio.config

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,8 +23,8 @@ export const config = {
     // will be called from there.
     //
     specs: [
-        // './test/specs/**/*.js'
-        './test/specs/api-test.js'
+        './test/specs/**/*.js'
+        // './test/specs/api-test.js'
         // './test/specs/ui-api-test.js' 
         // './test/specs/ui-test.js'
     ],
